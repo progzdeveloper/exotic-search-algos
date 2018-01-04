e8002ea9961637b9ad44518afcd1cf048ff7331e
progzdeveloper <progzdeveloper@gmail.com>
2018-01-04 21:58:56 +0300

    Update exponential search example

--- exponential_example.cpp
+++ exponential_example.cpp
@@ -19,23 +19,23 @@
 int main()
 {
     	using namespace std;
-	    // input data
-	    vector<string> vec = {
-		    "Hello", "from", "GCC", __VERSION__, "!" 
-	    };
+	// input data
+	vector<string> vec = {
+	    "Hello", "from", "GCC", __VERSION__, "!" 
+	};
 
-	    // sort
-	    sort(vec.begin(), vec.end());
+	// sort
+	sort(vec.begin(), vec.end());
 
-	    // print out the sorted data
-	    copy(vec.begin(), vec.end(), ostream_iterator<string>(cout, "\n"));
+	// print out the sorted data
+	copy(vec.begin(), vec.end(), ostream_iterator<string>(cout, "\n"));
 
-	    string key = "GCC"; // the key we are looking for
-	    auto it = exponential_search(vec.begin(), vec.end(), key);
-	    if (it != vec.end())
+	string key = "GCC"; // the key we are looking for
+	auto it = exponential_search(vec.begin(), vec.end(), key);
+	if (it != vec.end())
         	cout << "Found: '"<< key << "' at position: " << distance(vec.begin(), it) << endl;
     	else
-		      cout << '\''<< key << "' not found" << endl;
-	    return 0;
+		cout << '\''<< key << "' not found" << endl;
+	return 0;
 }
 
